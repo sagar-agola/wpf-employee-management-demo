b5dfabb37d10410638c30d0969bf4c575bb31148
Sagar Agola <N3o735@gmail.com>
2022-03-31 17:23:49 +0530

    style window from scretch

--- EmployeeManagement/MainWindow.xaml.cs
+++ EmployeeManagement/MainWindow.xaml.cs
@@ -24,5 +24,28 @@ namespace EmployeeManagement
         {
             InitializeComponent();
         }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                DragMove();
+            }
+        }
+
+        private void MinimizeCommand(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeCommand(object sender, RoutedEventArgs e)
+        {
+            WindowState ^= WindowState.Maximized;
+        }
+
+        private void CloseCommand(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
     }
 }
